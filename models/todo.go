b44a551746e3127f15f6e87a965d16862051dfce
cynicade <cynkvs@gmail.com>
2022-02-22 07:19:21 +0100

    Changed todo model

--- models/todo.go
+++ models/todo.go
@@ -1,12 +1,16 @@
 package models
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 )
 
 type Todo struct {
-	Id       int64     `json:"id"`
-	Body     string    `json:"body"`
-	Complete bool      `json:"complete" gorm:"default:false"`
-	User_ID  uuid.UUID `json:"-" gorm:"not null"`
+	Id        int64     `json:"id"`
+	Body      string    `json:"body"`
+	Complete  bool      `json:"complete" gorm:"default:false"`
+	CreatedAt time.Time `json:"created"`
+	UpdatedAt time.Time `json:"updated"`
+	User_ID   uuid.UUID `json:"-" gorm:"not null"`
 }
